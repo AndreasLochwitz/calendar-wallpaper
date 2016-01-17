f9c6fcb23522c9bcc88893e4f055fe1b3e909368
Andreas Lochwitz <andreas_lochwitz@live.de>
2016-01-17 21:14:05 +0100

    added aliasing for font display

--- calendarwallpaper.py
+++ calendarwallpaper.py
@@ -8,13 +8,15 @@ cal = calendar.Calendar()
 days = ['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So']
 i = 0;
 
-fnt = ImageFont.truetype('miso-light.ttf', 20)
+scaleFactor = 5
 
-fnt_gross = ImageFont.truetype('miso-regular.ttf', size=100)
+fnt = ImageFont.truetype('miso-light.ttf', 20 * scaleFactor)
+
+fnt_gross = ImageFont.truetype('miso-regular.ttf', size=100 * scaleFactor)
 
 source_image = Image.open("MGL-Logo-Air.png").convert('RGBA')
 
-text_image = Image.new('RGBA', source_image.size, (255,255,255,0))
+text_image = Image.new('RGBA', (source_image.size[0] * scaleFactor, source_image.size[1] * scaleFactor), (255,255,255,0))
 
 d = ImageDraw.Draw(text_image)
 
@@ -22,10 +24,10 @@ d = ImageDraw.Draw(text_image)
 
 d.fontmode = "1"
 
-x_pos = 10
-y_pos = 20
-delta_x = 40
-delta_y = 20
+x_pos = 10 * scaleFactor
+y_pos = 20 * scaleFactor
+delta_x = 40 * scaleFactor
+delta_y = 20 * scaleFactor
 alpha = 255
 
 for x in cal.itermonthdays(2016, 1):
@@ -42,13 +44,14 @@ for x in cal.itermonthdays(2016, 1):
 
     d.text((10, -20), 'Januar 2016', font=fnt_gross, fill=(255,255,255,50))
 
-text_image = text_image.filter(ImageFilter.SMOOTH)
+#text_image = text_image.filter(ImageFilter.SMOOTH)
+text_image = text_image.resize(source_image.size, Image.ANTIALIAS)
 
 #source_image = source_image.filter(ImageFilter.SMOOTH)
 
 out = Image.alpha_composite(source_image, text_image) #.filter(ImageFilter.GaussianBlur)
 
-out.show()
+out.save('bla.png');
 
 # source_image.show()
 
